6b86ed4c8a87e5cc49cb826cd0fdd4e0f6aafbab
Sashiki05 <save.cutolo@gmail.comm>
2023-06-21 13:39:00 +0200

    Express Quest 8 change

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ const welcome = (req, res) => {
 
 // ----------------------------Connection to server--------------------------
 app.use(express.json());
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
 
 app.get("/", welcome);
 
